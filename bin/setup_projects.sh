ee0f1f0436f4bcbdefe217d40e308d9d1eb6d0c2
james gibson <ox-prolifics.com@bastion.a3d4.example.opentlc.com>
2019-08-29 02:27:33 +0000

    updated GUID in Jenkinsfile

--- bin/setup_projects.sh
+++ bin/setup_projects.sh
@@ -22,7 +22,7 @@ if [ "$FROM_JENKINS" = "true" ]; then
   oc policy add-role-to-user admin ${USER} -n ${GUID}-tasks-dev
   oc policy add-role-to-user admin ${USER} -n ${GUID}-tasks-prod
 
-  oc annotate namespace ${GUID}-jenkins    openshift.io/requester=${USER} --overwrite=true
-  oc annotate namespace ${GUID}-tasks-dev  openshift.io/requester=${USER} --overwrite=true
-  oc annotate namespace ${GUID}-tasks-prod openshift.io/requester=${USER} --overwrite=true
+  oc annotate namespace ${GUID}-jenkins    openshift.io/requester=${USER} --overwrite
+  oc annotate namespace ${GUID}-tasks-dev  openshift.io/requester=${USER} --overwrite
+  oc annotate namespace ${GUID}-tasks-prod openshift.io/requester=${USER} --overwrite
 fi
