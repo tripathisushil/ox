258356d6075159df8d24e600dcd5df836026a8e3
james gibson <ox-prolifics.com@bastion.a3d4.example.opentlc.com>
2019-09-03 23:51:24 +0000

    Added initial pipeline funtionality and cleaned up codebase

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -13,14 +13,28 @@ CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
 # Set up Jenkins with sufficient resources
-oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
-oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m -n ${GUID}-jenkins
+# oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
+# oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m -n ${GUID}-jenkins
 
 # Create custom agent container image with skopeo
-oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\nUSER root\nRUN yum -y install skopeo && yum clean all\nUSER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
-oc get is -n ${GUID}-jenkins
+# oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\nUSER root\nRUN yum -y install skopeo && yum clean all\nUSER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
+# oc get is -n ${GUID}-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
+echo 'kind: "BuildConfig"
+apiVersion: "v1"
+metadata:
+   name: "homework-pipeline"
+spec:
+   source:
+      type: "Git"
+      git:
+         uri: "https://github.com/ox-prolifics/rhocp-appDeploy-homework.git"
+   strategy:
+      type: "JenkinsPipeline"
+      jenkinsPipelineStrategy:
+         jenkinsfilePath: openshift-tasks/Jenkinsfile' | oc create -f -
+oc start-build homework-pipeline
 
 # Make sure that Jenkins is fully up and running before proceeding!
 # while : ; do
