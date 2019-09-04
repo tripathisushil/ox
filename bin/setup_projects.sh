a71790bc20298f20aad71f09b38216f0560b7121
james gibson <ox-prolifics.com@bastion.a3d4.example.opentlc.com>
2019-09-04 16:25:18 +0000

    Trying to fix grading pipeline

--- bin/setup_projects.sh
+++ bin/setup_projects.sh
@@ -18,10 +18,12 @@ oc new-project ${GUID}-tasks-dev  --display-name="${GUID} AdvDev Homework Tasks 
 oc new-project ${GUID}-tasks-prod --display-name="${GUID} AdvDev Homework Tasks Production"
 
 if [ "$FROM_JENKINS" = "true" ]; then
+  oc policy add-role-to-user admin ${USER} -n gpte-jenkins
   oc policy add-role-to-user admin ${USER} -n ${GUID}-jenkins
   oc policy add-role-to-user admin ${USER} -n ${GUID}-tasks-dev
   oc policy add-role-to-user admin ${USER} -n ${GUID}-tasks-prod
 
+  oc annotate namespace gpte-jenkins       openshift.io/requester=${USER} --overwrite
   oc annotate namespace ${GUID}-jenkins    openshift.io/requester=${USER} --overwrite
   oc annotate namespace ${GUID}-tasks-dev  openshift.io/requester=${USER} --overwrite
   oc annotate namespace ${GUID}-tasks-prod openshift.io/requester=${USER} --overwrite
