e140f9550ddff7bdcd86387d8d90b5feac55647f
james gibson <ox-prolifics.com@bastion.a3d4.example.opentlc.com>
2019-09-04 04:36:34 +0000

    final update?

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -20,7 +20,20 @@ oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=
 oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\nUSER root\nRUN yum -y install skopeo && yum clean all\nUSER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
 oc get is -n ${GUID}-jenkins
 
+# Make sure that Jenkins is fully up and running before proceeding!
+while : ; do
+  echo "Checking if Jenkins is Ready..."
+  AVAILABLE_REPLICAS=$(oc get dc jenkins -n ${GUID}-jenkins -o=jsonpath='{.status.availableReplicas}')
+  if [[ "$AVAILABLE_REPLICAS" == "1" ]]; then
+    echo "...Yes. Jenkins is ready."
+    break
+  fi
+  echo "...no. Sleeping 10 seconds."
+  sleep 10
+done
+
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
+oc project ${GUID}-jenkins
 echo 'kind: "BuildConfig"
 apiVersion: "v1"
 metadata:
@@ -33,17 +46,6 @@ spec:
    strategy:
       type: "JenkinsPipeline"
       jenkinsPipelineStrategy:
-         jenkinsfilePath: openshift-tasks/Jenkinsfile' | oc create -f -
-oc start-build homework-pipeline
+         jenkinsfilePath: openshift-tasks/Jenkinsfile' | oc create -f - -n ${GUID}-jenkins
+oc start-build homework-pipeline -n ${GUID}-jenkins
 
-# Make sure that Jenkins is fully up and running before proceeding!
-# while : ; do
-#   echo "Checking if Jenkins is Ready..."
-#  AVAILABLE_REPLICAS=$(oc get dc jenkins -n ${GUID}-jenkins -o=jsonpath='{.status.availableReplicas}')
-#   if [[ "$AVAILABLE_REPLICAS" == "1" ]]; then
-#     echo "...Yes. Jenkins is ready."
-#     break
-#   fi
-#   echo "...no. Sleeping 10 seconds."
-#   sleep 10
-# done
